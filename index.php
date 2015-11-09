26209feac93d335992de85ebc8e259b571845483
DanyalXD <danyal1995@hotmail.co.uk>
2015-11-09 13:37:12 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+
+/**
+ * Created by PhpStorm.
+ * User: 1508228
+ * Date: 09/11/2015
+ * Time: 13:33
+ */
+<!DOCTYPE html>
+<html>
+<head lang="en">
+ <meta charset="UTF-8">
+ <title>Deployment Test</title>
+</head>
+<body>
+<H1> It's ALIVE!!!</H1>
+</body>
+</html>
